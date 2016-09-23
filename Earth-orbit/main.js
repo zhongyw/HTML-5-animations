59e99b6230a2fc83677734aa0fb53a65e71bf718
zhongyw <zw198307@163.com>
2016-09-23 16:11:43 +0800

    Earth-orbit

---
+++ Earth-orbit/main.js
@@ -0,0 +1,12 @@
+
+/* 随机添加背景中的星星*/
+var siteWidth = $(window).width();
+for (i = 0; i <= 100; i++) {
+  var start = $('<div>')
+  .addClass('star star_'+i)
+  .css({
+    top: Math.floor(Math.random()*500),
+    left: Math.floor(Math.random()*siteWidth)
+  })
+  .appendTo('.stars');
+}
